22381c28f6332a44504192c36e6ca6050912e1d8
Chris Michael <chrism@Chris-Mac.local>
2016-08-11 15:25:44 +0930

    Part 3 Completed - Added a UIPickerView to the Storyboard and connected it to the View Controller

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["ACT", "SA", "VIC", "NSW", "TAS", "NT", "QLD"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
     }
 
+    @IBAction func stateButtonPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: .normal)
+        statePicker.isHidden = true
+    }
+    
 }
 
